b62962fa76a55636dd01491587c19854589a7732
Teddy Zugana <teddy@ezeelink.co.id>
2025-10-29 09:37:17 +0700

    First Commit

---
+++ Tower.cs
@@ -0,0 +1,26 @@
+// C# recursive program to solve tower of hanoi puzzle
+using System;
+class GFG {
+    static void towerOfHanoi(int n, char from_rod,
+                             char to_rod, char aux_rod)
+    {
+        if (n == 0) {
+            return;
+        }
+        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+        Console.WriteLine("Move disk " + n + " from rod "
+                          + from_rod + " to rod " + to_rod);
+        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    }
+
+    //  Driver method
+    public static void Main(String[] args)
+    {
+        int N = 3;
+
+        // A, B and C are names of rods
+        towerOfHanoi(N, 'A', 'C', 'B');
+    }
+}
+
+// This code is contributed by shivanisinghss2110
